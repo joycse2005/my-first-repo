53e633135be1dd83f3c6b87b29efb7ab980a7bf6
joycse2005 <joybiswas2028@gmail.com>
2024-10-28 13:23:06 +0600

    first commit

---
+++ area-of-a-circle.cpp
@@ -0,0 +1,15 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     float r = 10;
+//     float a = 3.1416*r*r;
+//     cout<<"area is = ";
+//     cout<<a;
+
+//     float r=10;
+//     cout<<"area is = ";
+//     cout<<3.1416*r*r;
+
+
+// }
